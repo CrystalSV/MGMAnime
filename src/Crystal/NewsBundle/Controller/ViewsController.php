6cb515ca07f86ecd54e3768061f54c4fab502fee
skenji <marvin.vasquez@hotmail.es>
2013-02-01 00:30:23 -0600

    Empezando con index

--- src/Crystal/NewsBundle/Controller/ViewsController.php
+++ src/Crystal/NewsBundle/Controller/ViewsController.php
@@ -12,7 +12,12 @@ class ViewsController extends Controller
     	$em = $this->getDoctrine()->getEntityManager();
 
     	$BkNews = $em->getRepository('CrystalBaseBundle:ctrBreakingNews')->findAll();
+    	$i = 0;
+    	$News = array();
+    	foreach ($BkNews as $key => $value) {
+    		$News[$i] = $em->getRepository('CrystalBaseBundle:carNews')->find($value->idNew);
+    	}
 
-        return $this->render('CrystalNewsBundle:Default:index.html.twig', array('BkNews' => $BkNews));
+        return $this->render('CrystalNewsBundle:Default:index.html.twig', array('BkNews' => $BkNews, 'News' => $News));
     }
 }
