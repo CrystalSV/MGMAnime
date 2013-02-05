eb4b44de77f824216bcd0d666e951dd8376a9a16
Miguel Isaac Duarte Gutiérrez <mi99_link@hotmail.com>
2013-02-05 10:40:28 +0100

    clases para subir audio e imagen

---
+++ src/Crystal/NewsBundle/Resources/classes/audio.php
@@ -0,0 +1,63 @@
+<?php
+	namespace Crystal\rutasBundle\Resources\classes;
+
+	class audio
+	{
+		var $audio;
+
+		function __GET($name)
+		{
+			return $this->$name;
+		}
+
+		function __SET($name, $value)
+		{
+			$this->$name = $value;
+		}
+
+		function upload($name = '')
+		{
+			if($name == '')
+			{
+				$range = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+				for($i = 0; $i < 12; $i++)
+				{ 
+					$name .= substr($range, rand(0,62), 1);
+				}
+			}
+			
+			$folder = 'pics';
+			$type = explode('audio/', $this->audio->getClientMimeType());
+		
+			$file = $folder . '/' . $name .'.'. $type[1];
+			$this->img->move('audio', $file);
+			return $file;
+
+		}
+
+		function checkErrors()
+		{
+			$size = $this->audio->getsize();
+			$maxSize = '104857600';
+			if($size < $maxSize)
+			{
+				$fullType = explode('audio/', $this->img->getClientMimeType());
+				$type = $fullType[1];
+				if($type == 'mp3' || $type == 'ogg')
+				{
+					$error = 'NoError';
+				}
+				else
+				{
+					$error = 'Type Error';
+				}
+			}
+			else
+			{
+				$error = 'Size Error';
+			}
+			return $error;
+		}
+	}
+
+?>
